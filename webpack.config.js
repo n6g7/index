e33157054e52f2d938ab167f08fb9f67e24fff56
Nathan Gaberel <nathang@theodo.co.uk>
2018-03-17 14:50:04 +0000

    Update webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   entry: './index.js',
+  mode: process.env.NODE_ENV,
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
